1eb55b514059c7d703e0cbaddfbe64f9ec3c5919
Amaury Clochard <amaury.clochard@enseirb-matmeca.fr>
2022-11-17 12:04:16 +0100

    create routes for Game

--- backend/route.js
+++ backend/route.js
@@ -21,6 +21,26 @@ module.exports = app => {
 
     //Retrieve all Players + persons
     router.get("/players/", controller.findAllPlayers)
+    
+    
+    /*  Amaux (je dis ça juste si y'a des erreurs ici c'est ma faute) */
+    // Create a new Game
+    router.get("/game", controller.findAllGame);
+  
+    // Retrieve a single person with id
+    router.get("/game/:id", controller.findOneGame);
+  
+    // Update a person with id
+    router.put("/game/:id", controller.updateGame);
+  
+    // Delete a person with id
+    router.delete("/game/:id", controller.deleteGame);
+
+    //Retrieve all Players + persons
+    router.get("/game/", controller.findAllGame)
+    
+    /*  fin Amaux */
+
 
     app.use('/api/', router);
   };
